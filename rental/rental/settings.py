969f8c6f31c273b63ead1763173617cf98fd7c35
Viktor Gusakov <vitya.gusakov.00@gmail.com>
2025-04-17 13:39:27 +0300

    Delete files

--- rental/rental/settings.py
+++ rental/rental/settings.py
@@ -34,8 +34,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -136,7 +135,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
